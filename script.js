c1d69d48c6ff8f62ad363e9436d3c16aa963f77a
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-08-19 12:57:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,42 @@
 //your JS code here. If required.
-// Function to create a Promise that resolves after a random time between 1 and 3 seconds
-    function waitRandomTime() {
-      const min = 1000; // 1 second in milliseconds
-      const max = 3000; // 3 seconds in milliseconds
-      const randomTime = Math.floor(Math.random() * (max - min + 1)) + min;
-      return new Promise(resolve => setTimeout(resolve, randomTime));
-    }
+function delay(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
 
-    // Function to update the table with the results
-    function updateTable(results) {
-      const loadingText = document.getElementById("loading-text");
-      loadingText.parentNode.removeChild(loadingText);
+        const outputTable = document.getElementById('output');
+        const loadingRow = document.getElementById('loading-row');
 
-      // Update the table with the results of each Promise
-      for (let i = 0; i < results.length; i++) {
-        const promiseNumber = i + 1;
-        const timeInSeconds = (results[i] / 1000).toFixed(3);
-        const newRow = document.createElement("tr");
-        const promiseCell = document.createElement("td");
-        const timeCell = document.createElement("td");
-        promiseCell.textContent = `Promise ${promiseNumber}`;
-        timeCell.textContent = timeInSeconds;
-        newRow.appendChild(promiseCell);
-        newRow.appendChild(timeCell);
-        document.querySelector("table").appendChild(newRow);
-      }
+        const promises = [
+            delay(Math.random() * 2000 + 1000), // Between 1 and 3 seconds
+            delay(Math.random() * 2000 + 1000),
+            delay(Math.random() * 2000 + 1000)
+        ];
 
-      // Calculate the total time taken to resolve all promises
-      const totalTime = results.reduce((acc, curr) => acc + curr, 0);
-      const totalInSeconds = (totalTime / 1000).toFixed(3);
+        Promise.all(promises)
+            .then(() => {
+                outputTable.removeChild(loadingRow);
 
-      // Add the row for total time to the table
-      const totalRow = document.createElement("tr");
-      const totalCell = document.createElement("td");
-      const totalTimeCell = document.createElement("td");
-      totalCell.textContent = "Total";
-      totalTimeCell.textContent = totalInSeconds;
-      totalRow.appendChild(totalCell);
-      totalRow.appendChild(totalTimeCell);
-      document.querySelector("table").appendChild(totalRow);
-    }
+                promises.forEach((promise, index) => {
+                    const row = document.createElement('tr');
+                    const nameCell = document.createElement('td');
+                    const timeCell = document.createElement('td');
 
-    // Create three promises
-    const promises = [waitRandomTime(), waitRandomTime(), waitRandomTime()];
+                    nameCell.textContent = `Promise ${index + 1}`;
+                    timeCell.textContent = (promise / 1000).toFixed(3);
 
-    // Wait for all promises to resolve using Promise.all
-    Promise.all(promises)
-      .then(results => {
-        // Once all promises are resolved, update the table with the results
-        updateTable(results);
-      })
-      .catch(error => {
-        console.error("An error occurred:", error);
-      });
+                    row.appendChild(nameCell);
+                    row.appendChild(timeCell);
+                    outputTable.appendChild(row);
+                });
+
+                const totalRow = document.createElement('tr');
+                const totalNameCell = document.createElement('td');
+                const totalTimeCell = document.createElement('td');
+
+                totalNameCell.textContent = 'Total';
+                totalTimeCell.textContent = (promises.reduce((total, promise) => total + promise, 0) / 1000).toFixed(3);
+
+                totalRow.appendChild(totalNameCell);
+                totalRow.appendChild(totalTimeCell);
+                outputTable.appendChild(totalRow);
+            });
